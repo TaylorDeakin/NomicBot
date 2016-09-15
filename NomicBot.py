a99578e839a14420e914875f75598a098e3bd035
tdeakin <amles.gamer@gmail.com>
2016-09-16 10:14:20 +1200

    Initial Bot Loop

--- NomicBot.py
+++ NomicBot.py
@@ -1,5 +1,50 @@
 import time
 import praw
+import re
 
-r = praw.Reddit('/r/yet_another_nomic monitor bot by /u/KingAmles v 0.1'
-                'Url: github.com/TaylorDeakin/NomicBot')
+r = praw.Reddit('/r/yet_another_nomic monitor by /u/KingAmles v 0.1'
+                'Url: github.com/TaylorDeakin')
+
+r.login()
+
+# matches any three numbers in a row, ex 303
+pattern = re.compile('([0-9]){3}')
+
+already_done = []
+
+while True:
+    subreddit = r.get_subreddit('yet_another_nomic')
+    for submission in subreddit.get_hot(limit=10):
+        # if we haven't covered the submission already, and it matches at least one of these conditions
+        if submission.id not in already_done and ("Proposal" in submission.title or pattern.match(submission.title)):
+
+            op_text = submission.selftext.lower()
+
+            print(submission.title)
+            comments = submission.comments
+
+            seconder = ""
+            ayes = []
+            nays = []
+            flat_comments = praw.helpers.flatten_tree(comments)
+
+            # loop through the comments
+            for comment in flat_comments:
+                # accounting for discrepancies in caps by lowering text
+                commentText = comment.body.lower()
+                # make sure we're not duplicating entries
+                if "aye" in commentText and comment.author.name not in ayes:
+                    ayes.append(comment.author.name)
+                # make sure there isn't already a seconder
+                elif "seconded" in commentText and not seconder:
+                    seconder = comment.author
+                    ayes.append(comment.author.name)
+                # also make sure we're not duplicating entries
+                elif "nay" in commentText and comment.author.name not in nays:
+                    nays.append(comment.author.name)
+            # print stuff
+            print(seconder)
+            print(nays)
+            print(ayes)
+
+            already_done.append(submission.id)
